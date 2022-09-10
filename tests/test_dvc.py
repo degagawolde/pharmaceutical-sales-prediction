ef24eaf288544895964d9e1aa298d019e7c9088f
Degaga Wolde <39334921+degagawolde@users.noreply.github.com>
2022-09-10 19:17:17 +0300

    Update test_dvc.py

--- tests/test_dvc.py
+++ tests/test_dvc.py
@@ -16,7 +16,7 @@ dvc_load = DataLoader()
 # First load the cleaned stores data
 data_path = 'data/cleaned/store.csv'
 version = 'store_v2'
-repo = '../'
+repo = 'https://github.com/degagawolde/pharmaceutical-sales-prediction'
 
 store_df = dvc_load.dvc_get_data(data_path, version, repo)
 
