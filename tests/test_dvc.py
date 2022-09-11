948fdb5a594c1956da8effc2e74fbe85ac2c5c90
Degaga Wolde <39334921+degagawolde@users.noreply.github.com>
2022-09-11 10:01:05 +0300

    Update test_dvc.py

--- tests/test_dvc.py
+++ tests/test_dvc.py
@@ -16,7 +16,7 @@ dvc_load = DataLoader()
 # First load the cleaned stores data
 data_path = 'data/cleaned/store.csv'
 version = 'store_v2'
-repo = './'
+repo = '../'
 
 store_df = dvc_load.dvc_get_data(data_path, version, repo)
 
