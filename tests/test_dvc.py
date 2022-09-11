8db87a03791a3aa2177ce97f9c99d3dca91d7751
Degaga Wolde <39334921+degagawolde@users.noreply.github.com>
2022-09-11 19:39:06 +0300

    Update test_dvc.py

--- tests/test_dvc.py
+++ tests/test_dvc.py
@@ -15,19 +15,19 @@ dvc_load = DataLoader()
 
 # First load the cleaned stores data
 data_path = 'data/cleaned/store.csv'
-version = 'store_v2'
-repo = 'https://github.com/degagawolde/pharmaceutical-sales-prediction.git'
+version = 'eaf6d2e8828a365523b9b8800a79d45c6b236368'
+repo = 'https://github.com/degagawolde/pharmaceutical-sales-prediction'
 
 store_df = dvc_load.dvc_get_data(data_path, version, repo)
 
 # Then load the raw sales data
 data_path = 'data/merged/train.csv'
-version = 'train_v2'
+version = '2c43bd5e314858f9b692a7d67fc894e8c04b282a'
 train_df = dvc_load.dvc_get_data(data_path, version, repo)
 
 # Finally load the test data
 data_path = 'data/merged/test.csv'
-version = 'test_v2'
+version = 'ecc063bb453b6ca9e10fb2341d58ebbfe40b2514'
 test_df = dvc_load.dvc_get_data(data_path, version, repo)
 
 class TestGetInformations(unittest.TestCase):
